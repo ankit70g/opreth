7a6a9a1e54ac8e3b9c4aec3da2a28a6fc9cdd2b8
Ankit Gupta <ankitmanojg@gmail.com>
2026-01-13 13:29:36 +0530

    IDE layout
    Co-authored-by: Kajal Tiwari <kajaltiwari0902@gmail.comprojects>

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -7,7 +7,7 @@ import {
     Unauthenticated
 } from 'convex/react'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
-import { ClerkProvider, useAuth, UserButton } from '@clerk/nextjs'
+import { ClerkProvider, useAuth } from '@clerk/nextjs'
 import { ThemeProvider } from './theme-provider';
 import { UnauthenticatedView } from '@/features/auth/components/unauthenticated-view';
 import { AuthLoadingView } from '@/features/auth/components/auth-loading-view';
@@ -26,7 +26,6 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
                     disableTransitionOnChange
                 >
                     <Authenticated>
-                        <UserButton />
                         {children}
                     </Authenticated>
                     <Unauthenticated>
